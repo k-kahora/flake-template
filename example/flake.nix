d16a8a767ce6ad8848e5cff15b60000f911dcd46
k-kahora <mango22mjk@gmail.com>
2023-09-27 18:38:16 -0400

    prolog flake

--- example/flake.nix
+++ example/flake.nix
@@ -3,16 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.default = pkgs.writeShellScriptBin "example" ''
-        echo "hello world"
-      '';
-    };
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.writeShellScriptBin "example" ''
+          echo "hello world"
+        '';
+        devShells.default = mkShell {
+          buildInputs = with pkgs; [swiProlog];
+          shellHook = "alias prolog=swipl";
+        };
+      };
 }
