d9213a0fe3db06b843b567cb0dbe93d1adb10766
k-kahora <mango22mjk@gmail.com>
2023-10-22 11:18:10 -0400

    Wrong flake file updated
    This reverts commit 4703cbcef80abcee014eb9a07f58af67d3f88aec.

--- flake.nix
+++ flake.nix
@@ -1,41 +1,19 @@
 {
-  description = "o-caml flake";
+  description = "A simple flake temlpate";
+  outputs = {self}: {
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    # Do precise filtering of files in the nix store
-    nix-filter.url = "github:numtide/nix-filter";
+    templates = {
+      ocaml = {
+        path = ./ocaml;
+        description = "Basic ocaml shell flake";
+      };
+      example = {
+        path = ./example;
+        description = "A super basic flake";
+      };
+    };
+
+    defaultTemplate = self.templates.example;
   };
-
-  outputs = { self, nixpkgs, flake-utils, nix-filter}:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        legacyPackages = nixpkgs.legacyPackages.${system};
-        ocamlPackages = legacyPackages.ocamlPackages;
-        lib = legacyPackages.lib;
-      in
-      {
-      devShells = {
-        default = legacyPackages.mkShell {
-          packages = [
-
-            legacyPackages.nixpkgs-fmt
-            legacyPackages.ocamlformat
-            legacyPackages.fswatch
-            ocamlPackages.odoc
-            ocamlPackages.ocaml-lsp
-            ocamlPackages.ocamlformat-rpc-lib
-            ocamlPackages.utop
-            ocamlPackages.core
-            ocamlPackages.batteries
-            ocamlPackages.dune_3
-            ocamlPackages.ocaml
-
-          ];
-        };
-
-       };
-     }
-    );
 }
+
