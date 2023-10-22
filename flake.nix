de0bd9aebae463d94883bf8ea021b7de147eb2d9
k-kahora <mango22mjk@gmail.com>
2023-10-22 11:21:39 -0400

    template description

--- flake.nix
+++ flake.nix
@@ -6,6 +6,11 @@
       ocaml = {
         path = ./ocaml;
         description = "Basic ocaml shell flake";
+        welcomText = ''
+          # Simple dev shell for ocaml
+          ## Intended use
+          Starting up basic ocaml projects
+        '';
       };
       example = {
         path = ./example;
